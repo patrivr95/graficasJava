a05a8b7eaa8701577c7a045fce36622917c5b290
patrivr95 <patriciavadilloruiz@gmail.com>
2023-06-15 10:31:07 +0200

    eventos

---
+++ 01_primera_ventana/src/presentacion/Inicio.java
@@ -0,0 +1,12 @@
+package presentacion;
+
+import presentacion.graficos.MiVentana;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+		new MiVentana();
+
+	}
+
+}
--- 02_eventos/src/presentacion/graficos/MiVentana.java
+++ 02_eventos/src/presentacion/graficos/MiVentana.java
@@ -1,7 +1,11 @@
 package presentacion.graficos;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class MiVentana extends JFrame {
 	public MiVentana() {
@@ -10,7 +14,7 @@ public class MiVentana extends JFrame {
 		// modificar el comportamiento del botón de cierre
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		// tamaño y posición
-		this.setBounds(120, 50, 400, 300);
+		this.setBounds(120, 50, 800, 500);
 		// proceso de creación de controles gráficos
 		inicializarComponentes();
 		// visualizar ventana. Esta tiene que ser la última
@@ -22,10 +26,23 @@ public class MiVentana extends JFrame {
 		this.setLayout(null);
 		// creamos el botón y le damos texto
 		JButton jb1=new JButton("Pulsar aquí");
+		// instanciamos JLabel
+		JLabel jl1 = new JLabel();
 		// tamaño y posición
 		jb1.setBounds(150, 80, 150, 30);
+		jl1.setBounds(400, 80, 200, 30);
 		// añadimos el botón a la ventana
 		this.add(jb1);
-		
+		this.add(jl1);
+		// eventos
+		/*ActionListener action = new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				jl1.setText("Primer evento");
+			}
+		};*/
+		// evento con expresion lambda
+		ActionListener action = e->jl1.setText("Primer evento");
 	}
 }
